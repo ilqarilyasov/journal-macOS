8381de75596d1a11fb7d4f3544fa0a0d6b1bd2f8
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-12-06 12:17:28 -0600

    Initial Commit

---
+++ Journal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Journal
+//
+//  Created by Ilgar Ilyasov on 12/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
